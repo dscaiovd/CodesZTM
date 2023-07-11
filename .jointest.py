d573bf17b5cf09ad9621a2aac73e640a15e78841
Caio Vividi <139068105+dscaiovd@users.noreply.github.com>
2023-07-11 02:44:59 -0300

    changing language
    just to look easier to understand

--- .jointest.py
+++ .jointest.py
@@ -1,8 +1,8 @@
-country_list = input("Digite os países visitados separados por espaço: ")
+country_list = input("Insert the countries you had visit separeted by space: ")
 separeted_list = country_list.split(' ')
 
 capitalize_country = [country.capitalize() for country in separeted_list]
 
 joined_list = ' - '.join(capitalize_country)
 
-print('Os países visitados são:', joined_list)
+print('The visited countries are:', joined_list)
