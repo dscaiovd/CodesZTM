2f3895223ea7d8fceefbbc75d8013e934bff0cca
dscaiovd <139068105+dscaiovd@users.noreply.github.com>
2023-07-20 05:24:39 -0300

    finding duplicates
    here I try to find duplicate values on the list without using set

---
+++ PythonBasics/findDuplicates.py
@@ -0,0 +1,8 @@
+some_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
+
+duplicates = []
+for i in some_list:
+    if some_list.count(i) > 1:
+        if i not in duplicates:
+            duplicates.append(i)
+print(duplicates)
